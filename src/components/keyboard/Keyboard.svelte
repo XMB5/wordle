8fe6daaa62d6ecad0d330fdffe981fcc6ebd0e49
Sam Foxman <elasticvine@protonmail.com>
2022-03-03 16:56:48 -0500

    Disable key colors

--- src/components/keyboard/Keyboard.svelte
+++ src/components/keyboard/Keyboard.svelte
@@ -49,7 +49,7 @@
 			<Key
 				{letter}
 				on:keystroke={(e) => appendValue(e.detail)}
-				state={$letterStates[letter]}
+				state={/*$letterStates[letter]*/'🔳'}
 			/>
 		{/each}
 	</div>
@@ -58,7 +58,7 @@
 			<Key
 				{letter}
 				on:keystroke={(e) => appendValue(e.detail)}
-				state={$letterStates[letter]}
+				state={/*$letterStates[letter]*/'🔳'}
 			/>
 		{/each}
 	</div>
@@ -68,7 +68,7 @@
 			<Key
 				{letter}
 				on:keystroke={(e) => appendValue(e.detail)}
-				state={$letterStates[letter]}
+				state={/*$letterStates[letter]*/'🔳'}
 			/>
 		{/each}
 		<Key letter="" on:keystroke={backspaceValue}>
